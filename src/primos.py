ca97de21650ab7d58a8d9d96aebba28dd1e9c7aa
Kiri-1 <belenjesus20@gmail.com>
2023-03-29 01:41:50 -0300

    agregando comentarios a primos.py

--- src/primos.py
+++ src/primos.py
@@ -1,7 +1,10 @@
-# prime number calculator: find all primes up to n
+
+#prime number calculator: find all primes up to n
+#encuentra todos los primos hasta el numero n
 max = int(input("Find primes up to what number? : "))
 primeList = []
 #for loop for checking each number
+#revisa cada numero
 for x in range(2, max + 1):
 	isPrime = True
 	index = 0
@@ -16,6 +19,7 @@ for x in range(2, max + 1):
 print(primeList)
 #-------------------------------------------------------------
 # prime number calculator: find the first n primes
+#encuentra los primeros numeros primos
 count = int(input("Find how many primes?: "))
 primeList = []
 x = 2
