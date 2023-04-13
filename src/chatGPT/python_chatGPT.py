b9250aa28504018e9ce6fa47c736ce60c9ddb199
Kiri-1 <belenjesus20@gmail.com>
2023-04-13 15:25:35 -0300

    agregando comversacion

--- src/chatGPT/python_chatGPT.py
+++ src/chatGPT/python_chatGPT.py
@@ -1,51 +1,75 @@
 
 # Escriba un programa en lenguaje Python que basándose en el esqueleto previamente proporcionado acepte una consulta del usuario, verifique si la misma tiene texto, imprima su contenido, invoque el API de chatGPT con esa consulta e imprima en pantalla el resultado que se obtenga como respuesta. El contenido de la consulta debe agregársele “You:” antes de imprimirlo y de enviarlo. La respuesta de chatGPT deberá agregársele “chatGPT: “antes de imprimirse.
 
-# https://platform.openai.com/docs/api-reference //documentacion 
+# https://platform.openai.com/docs/api-reference //documentacion. 
 
 import openai
+import argparse
 
-openai.api_key = "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"
+# Configuración de argumentos de la línea de comando.
+parser = argparse.ArgumentParser(description='Programa de chat con GPT-3')
+parser.add_argument('--convers', action='store_true', help='Habilita el modo de conversación')
+args = parser.parse_args()
 
-while True:
-    try:
-        # Se llama prompt al carácter o conjunto de caracteres que se muestran en una línea de comandos para indicar que está a la espera de órdenes.
-        print ('Ingrese una pregunta a chatGPT -- exit para finalizar')
-        prompt = input("\nYou: ") # El sistema operativo trae a travez del imput permitir la funcionalidad de "cursor Up".
+openai.api_key = "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"
 
-        if prompt == "":
-            continue
-        # Se detiene el programa cuando ingresa un exit al prompt.
-        # .lower() Convierte una cadena de caracteres a minúsculas.
-        if prompt.lower() == "exit" :
-            print("Hasta pronto!")
-            break
+# Parámetros de la API.
+TOP_P=1
+FREQ_PENALTY=0
+PRES_PENALTY=0
+STOP=None
+MAX_TOKENS=1024
+TEMPERATURE=0.75
+NMAX=1
+MODEL_ENGINE = "text-davinci-003"
 
-        # Agregamos "You:" al inicio de la consulta.
-        prompt = "You: " + prompt
+# Buffer para almacenar las consultas y respuestas anteriores.
+conver_buffer = []
 
-        TOP_P=1
-        FREQ_PENALTY=0
-        PRES_PENALTY=0
-        STOP=None
-        MAX_TOKENS=1024
-        TEMPERATURE=0.75
-        NMAX=1
-        MODEL_ENGINE = "text-davinci-003"
+# Función para imprimir el contenido de la consulta o la respuesta.
+def print_message(message, is_user_message=True):
+    prefix = "You: " if is_user_message else "chatGPT: "
+    print(prefix + message)
 
-        completion = openai.Completion.create(engine=MODEL_ENGINE,
-                                          prompt=prompt,
-                                          max_tokens=MAX_TOKENS,
-                                          n=NMAX,
-                                          top_p=TOP_P,
-                                          frequency_penalty=FREQ_PENALTY,
-                                          presence_penalty=PRES_PENALTY,
-                                          temperature=TEMPERATURE,
-                                          stop=STOP)
+print("Bienvenido a chatGPT")
+# Bucle principal para el modo de conversación.
+while args.convers:
+    user_input = input("Ingrese su consulta a chatGPT: ")
     
-        # Agregamos "chatGPT: " al inicio de la respuesta.
-        response = "chatGPT: " + completion.choices[0].text
-        print(response) # Imprime la respuesta.
+    # Si la consulta es nula, se ignora y se vuelve a pedir.
+    if not user_input:
+        continue
+    
+    # Salir del modo conversación al ingresar "salir".
+    if user_input == "salir":
+        break
+    
+    # Agregar la consulta al buffer de conversación.
+    conver_buffer.append("You: " + user_input)
+    
+    # Combinar las consultas anteriores con la última para enviar a la API.
+    prompt = "\n".join(conver_buffer)
+    
+    try:
+        # Invocar la API de chatGPT con la consulta actual y el buffer completo.
+        completion = openai.Completion.create(
+            engine=MODEL_ENGINE,
+            prompt=prompt,
+            max_tokens=MAX_TOKENS,
+            n=NMAX,
+            top_p=TOP_P,
+            frequency_penalty=FREQ_PENALTY,
+            presence_penalty=PRES_PENALTY,
+            temperature=TEMPERATURE,
+            stop=["You:", "chatGPT:"] # Indicar la cadena de STOP para el modo de conversación.
+        )
+        
+        # Agregar la respuesta al buffer de conversación.
+        response = completion.choices[0].text.strip()
+        conver_buffer.append("chatGPT: " + response)
+        
+        # Imprimir la respuesta de chatGPT.
+        print_message(response, False)
 
     # Agregue al programa anterior estructuras Try:/Except: para gestionar problemas en la ejecución, coloque un nido para la aceptación de consulta desde el usuario, otro para su tratamiento y un tercero para la invocación.
     
