e00df2d951bf978dfffe8b01a42d15abbb781074
Kiri-1 <belenjesus20@gmail.com>
2023-04-13 19:53:19 -0300

    aumentando pylint 0.24

--- src/chatGPT/python_chatGPT.py
+++ src/chatGPT/python_chatGPT.py
@@ -1,11 +1,11 @@
-# https://platform.openai.com/docs/api-reference //documentacion. 
+# https://platform.openai.com/docs/api-reference //documentacion.
 import openai
 import argparse
 # Configuración de argumentos de la línea de comando.
-parser = argparse.ArgumentParser(description='Programa de chat con GPT-3')
+parser=argparse.ArgumentParser(description='Programa de chat con GPT-3')
 parser.add_argument('--convers', action='store_true', help='Habilita el modo de conversación')
-args = parser.parse_args()
-openai.api_key = "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"
+args=parser.parse_args()
+openai.api_key="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"
 # Parámetros de la API.
 TOP_P=1
 FREQ_PENALTY=0
@@ -14,13 +14,13 @@ STOP=None
 MAX_TOKENS=1024
 TEMPERATURE=0.75
 NMAX=1
-MODEL_ENGINE = "text-davinci-003"
+MODEL_ENGINE="text-davinci-003"
 # Buffer para almacenar las consultas y respuestas anteriores.
-conver_buffer = []
+conver_buffer=[]
 # Función para imprimir el contenido de la consulta o la respuesta.
 def print_message(message, is_user_message=True):
-    prefix = "You:" if is_user_message else "chatGPT:"
-    print(prefix + message)
+    prefix="You:" if is_user_message else "chatGPT:"
+    print(prefix+message)
 print("Bienvenido a chatGPT")
 # Bucle principal para el modo de conversación.
 while args.convers:
@@ -34,10 +34,10 @@ while args.convers:
     # Agregar la consulta al buffer de conversación.
     conver_buffer.append("You:"+user_input)
     # Combinar las consultas anteriores con la última para enviar a la API.
-    prompt = "\n".join(conver_buffer)
+    prompt="\n".join(conver_buffer)
     try:
         # Invocar la API de chatGPT con la consulta actual y el buffer completo.
-        completion = openai.Completion.create(
+        completion=openai.Completion.create(
             engine=MODEL_ENGINE,
             prompt=prompt,
             max_tokens=MAX_TOKENS,
@@ -49,7 +49,7 @@ while args.convers:
             stop=["You:", "chatGPT:"] # Indicar la cadena de STOP para el modo de conversación.
         )
         # Agregar la respuesta al buffer de conversación.
-        response = completion.choices[0].text.strip()
+        response=completion.choices[0].text.strip()
         conver_buffer.append("chatGPT:"+response)
         # Imprimir la respuesta de chatGPT.
         print_message(response,False)
